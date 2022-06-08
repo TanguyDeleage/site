1c5ef860921b81cf57748aa585e8a9000b245794
tanguy deleage <tanguy.deleage0@gmail.com>
2022-06-08 14:03:36 +0200

    Header responsive

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+
+// Change le display de la navbar (none/flex) quand on clique sur le menu burger
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+
+  // Ouvre le dropdown menu et le ferme en cliquant sur l'icône
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+
+    burger.classList.toggle('toggle');
+  });
+
+  // Ferme le dropdown menu quand je clique sur un lien
+  nav.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+    burger.classList.toggle('toggle');
+  });
+}
+
+
+navSlide();
