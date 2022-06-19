4b644066edac3c5ff0c47f16f77fa93f65656828
tanguy deleage <tanguy.deleage0@gmail.com>
2022-06-19 18:06:20 +0200

    Rework with Js to colour portfolio projects on hover, but need to be clean with a forEach

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,26 +18,84 @@ const navSlide = () => {
   });
 }
 
-// A travailler
-const colorPortfolio = () => {
-  const project = document.querySelectorAll('.portfolio-project');
-  const picture = document.querySelectorAll('.portfolio-picture');
 
-  // project.addEventListener('mouseenter', () => {
-  //   picture.classList.toggle('grey');
-  // });
+// Matematch Portfolio - A optimiser avec un for each
+const colorPortfolioMatematch = () => {
 
-  project.forEach(function (element) {
-  element.addEventListener('mouseenter', () => {
-      picture.classList.toggle('grey');
-    });
+  const matematchProject = document.getElementById('matematch-portfolio-project');
+  const matematchPicture = document.getElementById('matematch-portfolio-picture');
+
+   // On hover change project color from grey to color
+  matematchProject.addEventListener('mouseenter', () => {
+    matematchPicture.classList.toggle('grey');
   });
 
-  project.addEventListener('mouseleave', () => {
-    picture.classList.toggle('grey');
+   // When the mouse leave the project div, the picture goes back to grey
+  matematchProject.addEventListener('mouseleave', () => {
+    matematchPicture.classList.toggle('grey');
   });
-}
+
+};
+
+
+// Daily UI Portfolio - A optimiser avec un for each
+const colorPortfolioDailyUi = () => {
+
+  const dailyUiProject = document.getElementById('daily-ui-portfolio-project');
+  const dailyUiPicture = document.getElementById('daily-ui-portfolio-picture');
+
+  // On hover change project color from grey to color
+  dailyUiProject.addEventListener('mouseenter', () => {
+    dailyUiPicture.classList.toggle('grey');
+  });
+  // When the mouse leave the project div, the picture goes back to grey
+  dailyUiProject.addEventListener('mouseleave', () => {
+    dailyUiPicture.classList.toggle('grey');
+  });
+
+};
+
+
+// Rallye Portfolio - A optimiser avec un for each
+const colorPortfolioRallye = () => {
+
+  const rallyeProject = document.getElementById('rallye-portfolio-project');
+  const rallyePicture = document.getElementById('rallye-portfolio-picture');
+
+  // On hover change project color from grey to color
+  rallyeProject.addEventListener('mouseenter', () => {
+    rallyePicture.classList.toggle('grey');
+  });
+
+   // When the mouse leave the project div, the picture goes back to grey
+  rallyeProject.addEventListener('mouseleave', () => {
+    rallyePicture.classList.toggle('grey');
+  });
+
+};
+
+
+// Coop Portfolio - A optimiser avec un for each
+const colorPortfolioCoop = () => {
+
+  const coopProject = document.getElementById('coop-portfolio-project');
+  const coopPicture = document.getElementById('coop-portfolio-picture');
+
+  // On hover change project color from grey to color
+  coopProject.addEventListener('mouseenter', () => {
+    coopPicture.classList.toggle('grey');
+  });
+
+   // When the mouse leave the project div, the picture goes back to grey
+  coopProject.addEventListener('mouseleave', () => {
+    coopPicture.classList.toggle('grey');
+  });
+
+};
 
 
 navSlide();
-colorPortfolio();
+colorPortfolioMatematch();
+colorPortfolioDailyUi();
+colorPortfolioRallye();
+colorPortfolioCoop();
