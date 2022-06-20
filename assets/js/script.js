b2aafa4ddd2de0ea9f139034822e44961e302f32
tanguy deleage <tanguy.deleage0@gmail.com>
2022-06-20 19:04:37 +0200

    Improved the scroll in the portfolio section to center the second row + Refactoring JS to remove the grayscale filter on the projects on hover in the portfolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,82 +20,62 @@ const navSlide = () => {
 
 
 // Matematch Portfolio - A optimiser avec un for each
-const colorPortfolioMatematch = () => {
+// const colorPortfolioMatematch = () => {
 
-  const matematchProject = document.getElementById('matematch-portfolio-project');
-  const matematchPicture = document.getElementById('matematch-portfolio-picture');
+//   const matematchProject = document.getElementById('matematch-portfolio-project');
+//   const matematchPicture = document.getElementById('matematch-portfolio-picture');
 
-   // On hover change project color from grey to color
-  matematchProject.addEventListener('mouseenter', () => {
-    matematchPicture.classList.toggle('grey');
-  });
+//    // On hover change project color from grey to color
+//   matematchProject.addEventListener('mouseenter', () => {
+//     matematchPicture.classList.toggle('grey');
+//   });
 
-   // When the mouse leave the project div, the picture goes back to grey
-  matematchProject.addEventListener('mouseleave', () => {
-    matematchPicture.classList.toggle('grey');
-  });
+//    // When the mouse leave the project div, the picture goes back to grey
+//   matematchProject.addEventListener('mouseleave', () => {
+//     matematchPicture.classList.toggle('grey');
+//   });
 
-};
+// };
 
+const colorPortfolio = () => {
 
-// Daily UI Portfolio - A optimiser avec un for each
-const colorPortfolioDailyUi = () => {
-
-  const dailyUiProject = document.getElementById('daily-ui-portfolio-project');
-  const dailyUiPicture = document.getElementById('daily-ui-portfolio-picture');
+  const project = document.querySelectorAll('.portfolio-project');
+  const picture = document.querySelectorAll('.portfolio-picture');
 
   // On hover change project color from grey to color
-  dailyUiProject.addEventListener('mouseenter', () => {
-    dailyUiPicture.classList.toggle('grey');
+  project[0].addEventListener('mouseenter', () => {
+    picture[0].classList.toggle('grey');
   });
+  project[1].addEventListener('mouseenter', () => {
+    picture[1].classList.toggle('grey');
+  });
+  project[2].addEventListener('mouseenter', () => {
+    picture[2].classList.toggle('grey');
+  });
+  project[3].addEventListener('mouseenter', () => {
+    picture[3].classList.toggle('grey');
+  });
+
   // When the mouse leave the project div, the picture goes back to grey
-  dailyUiProject.addEventListener('mouseleave', () => {
-    dailyUiPicture.classList.toggle('grey');
+  project[0].addEventListener('mouseleave', () => {
+    picture[0].classList.toggle('grey');
   });
-
-};
-
-
-// Rallye Portfolio - A optimiser avec un for each
-const colorPortfolioRallye = () => {
-
-  const rallyeProject = document.getElementById('rallye-portfolio-project');
-  const rallyePicture = document.getElementById('rallye-portfolio-picture');
-
-  // On hover change project color from grey to color
-  rallyeProject.addEventListener('mouseenter', () => {
-    rallyePicture.classList.toggle('grey');
+  project[1].addEventListener('mouseleave', () => {
+    picture[1].classList.toggle('grey');
   });
-
-   // When the mouse leave the project div, the picture goes back to grey
-  rallyeProject.addEventListener('mouseleave', () => {
-    rallyePicture.classList.toggle('grey');
+  project[2].addEventListener('mouseleave', () => {
+    picture[2].classList.toggle('grey');
   });
-
-};
-
-
-// Coop Portfolio - A optimiser avec un for each
-const colorPortfolioCoop = () => {
-
-  const coopProject = document.getElementById('coop-portfolio-project');
-  const coopPicture = document.getElementById('coop-portfolio-picture');
-
-  // On hover change project color from grey to color
-  coopProject.addEventListener('mouseenter', () => {
-    coopPicture.classList.toggle('grey');
-  });
-
-   // When the mouse leave the project div, the picture goes back to grey
-  coopProject.addEventListener('mouseleave', () => {
-    coopPicture.classList.toggle('grey');
+  project[3].addEventListener('mouseleave', () => {
+    picture[3].classList.toggle('grey');
   });
 
 };
 
 
 navSlide();
-colorPortfolioMatematch();
-colorPortfolioDailyUi();
-colorPortfolioRallye();
-colorPortfolioCoop();
+// colorPortfolioMatematch();
+// colorPortfolioDailyUi();
+// colorPortfolioRallye();
+// colorPortfolioCoop();
+colorPortfolio();
