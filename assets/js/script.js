809ca3312c955fc960ec3406ed753c811c630baa
tanguy deleage <tanguy.deleage0@gmail.com>
2022-10-28 17:06:06 +0200

    Rework on all js animations + fixing some colors/layout issue or improvement

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,60 +50,132 @@ const portfolioGreyToColor = () => {
 // Slide JS sur la home
 const slideInJsHome = () => {
 
-  // Services section slideIn
-  const services1Target = 720;
-  const services2Target = 780;
-  const services3Target = 840;
+  // Benefits section slideIn
+  const benefits1Target = 250;
+  const benefits2Target = 400;
+  const benefits3Target = 500;
 
-  const services1 = document.querySelector('.visibility');
-  const services2 = document.querySelector('.credibility');
-  const services3 = document.querySelector('.efficacity');
+  const benefits1 = document.querySelector('.visibility');
+  const benefits2 = document.querySelector('.credibility');
+  const benefits3 = document.querySelector('.efficacity');
 
   if (window.location.pathname == '/') {
     window.addEventListener("scroll", function () {
       const scrollUser = this.scrollY || window.pageYOffset;
 
-      if (scrollUser > services1Target) {
-        services1.classList.remove('slide-up-js');
+      if (scrollUser > benefits1Target) {
+        benefits1.classList.remove('slide-up-js');
       };
-      if (scrollUser > services2Target) {
-        services2.classList.remove('slide-up-js');
+      if (scrollUser > benefits2Target) {
+        benefits2.classList.remove('slide-up-js');
       };
-      if (scrollUser > services3Target) {
-        services3.classList.remove('slide-up-js');
+      if (scrollUser > benefits3Target) {
+        benefits3.classList.remove('slide-up-js');
+      };
+    });
+
+
+    // Offers section slideIn
+    const offersTarget = 1000;
+
+
+    const offer1 = document.querySelector('.self');
+    const offer2 = document.querySelector('.agency');
+
+
+
+    window.addEventListener("scroll", function () {
+      const scrollUser = this.scrollY || window.pageYOffset;
+
+      if (scrollUser > offersTarget) {
+        offer1.classList.remove('slide-left-js');
+      };
+      if (scrollUser > offersTarget) {
+        offer2.classList.remove('slide-right-js');
+      };
+    });
+
+
+    // Services section slideIn
+    const service1Target = 1750;
+    const service2Target = 1900;
+
+    const service1 = document.querySelector('.audit');
+    const service2 = document.querySelector('.build');
+
+
+    window.addEventListener("scroll", function () {
+      const scrollUser = this.scrollY || window.pageYOffset;
+
+      if (scrollUser > service1Target) {
+        service1.classList.remove('slide-up-js');
+      };
+      if (scrollUser > service2Target) {
+        service2.classList.remove('slide-up-js');
+      };
+    });
+
+
+    // Portfolio section slideIn
+    const portfolioRow1Target = 2600;
+    const portfolioRow2Target = 2650;
+
+    const portfolioCard1 = document.querySelector('.portfolio-card-1');
+    const portfolioCard2 = document.querySelector('.portfolio-card-2');
+    const portfolioCard3 = document.querySelector('.portfolio-card-3');
+    const portfolioCard4 = document.querySelector('.portfolio-card-4');
+
+    window.addEventListener("scroll", function () {
+      const scrollUser = this.scrollY || window.pageYOffset;
+
+     if (scrollUser > portfolioRow1Target) {
+       portfolioCard1.classList.remove('slide-right-js');
+       portfolioCard2.classList.remove('slide-right-js');
+     };
+
+     if (scrollUser > portfolioRow2Target) {
+       portfolioCard3.classList.remove('slide-left-js');
+       portfolioCard4.classList.remove('slide-left-js');
+     };
+    });
+
+    // Methodology section slideIn
+
+    const methodoTarget = 3600;
+    // const steps = document.querySelectorAll('.methodology-step')
+    const step1 = document.querySelector('.step-1');
+    const step2 = document.querySelector('.step-2');
+    const step3 = document.querySelector('.step-3');
+    const step4 = document.querySelector('.step-4');
+    const step5 = document.querySelector('.step-5');
+    const step6 = document.querySelector('.step-6');
+
+
+    window.addEventListener("scroll", function () {
+      const scrollUser = this.scrollY || window.pageYOffset;
+
+      if (scrollUser > methodoTarget) {
+        // steps.forEach(step => {
+        //   step.classList.remove('opacity-js');
+        // });
+
+        step1.classList.remove('opacity-js');
+        step2.classList.remove('opacity-js');
+        step3.classList.remove('opacity-js');
+        step4.classList.remove('opacity-js');
+        step5.classList.remove('opacity-js');
+        step6.classList.remove('opacity-js');
       };
     });
   };
-
-  // Portfolio section slideIn
-  const portfolioRow1Target = 2150
-  const portfolioRow2Target = 2200
-
-  const portfolioCard1 = document.querySelector('.portfolio-card-1');
-  const portfolioCard2 = document.querySelector('.portfolio-card-2');
-  const portfolioCard3 = document.querySelector('.portfolio-card-3');
-  const portfolioCard4 = document.querySelector('.portfolio-card-4');
-
-  window.addEventListener("scroll", function () {
-    const scrollUser = this.scrollY || window.pageYOffset;
-
-    if (scrollUser > portfolioRow1Target) {
-      portfolioCard1.classList.remove('slide-right-js');
-      portfolioCard2.classList.remove('slide-right-js');
-    };
-
-    if (scrollUser > portfolioRow2Target) {
-      portfolioCard3.classList.remove('slide-left-js');
-      portfolioCard4.classList.remove('slide-left-js');
-    };
-  });
 };
+
 
 const slideInJsAbout = () => {
 
-  const skill1Target = 520;
-  const skill2Target = 600;
-  const skill3Target = 680;
+  const skill1Target = 330;
+  const skill2Target = 430;
+  const skill3Target = 530;
 
   const skill1 = document.querySelector('.consultant');
   const skill2 = document.querySelector('.designer');
